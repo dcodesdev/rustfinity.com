9d26570ae7e1706fcccbad3dd989f0b566658b99
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-06-04 12:48:41 +0300

    version check

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,5 +1,6 @@
 mod challenge;
 mod cli;
+mod crates_io;
 mod download;
 
 use clap::Parser;
@@ -7,6 +8,17 @@ use cli::{run, Cli};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let latest_version = crates_io::get_latest_version().await?;
+    let current_version = env!("CARGO_PKG_VERSION");
+
+    if latest_version != current_version {
+        println!(
+            "A new version of rustfinity ({}) is available, please run the following command and try again:\n\n$ cargo install rustfinity",
+            latest_version
+        );
+        return Ok(());
+    }
+
     let cli = Cli::parse();
     run(cli).await
 }
