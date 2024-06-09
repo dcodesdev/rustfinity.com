e0a369606527bca5e34c7d1b67bd35c07e7993b9
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-06-09 08:18:40 +0300

    constants challenge

---
+++ challenges/constants/src/starter.rs
@@ -0,0 +1,6 @@
+// Define a constant MAX_SIZE with a value of 100.
+// Your code here
+
+pub fn main() -> i32 {
+    MAX_SIZE
+}
---
+++ challenges/constants/tests/tests.rs
@@ -0,0 +1,42 @@
+#[cfg(test)]
+mod tests {
+    use constants::*;
+    use syntest::Syntest;
+
+    #[test]
+    fn compiles() {
+        assert_eq!(main(), 100, "Expected main() to return 100");
+    }
+
+    #[test]
+    fn test_constants() {
+        let syntest = Syntest::from("./src/lib.rs");
+
+        let const_to_exist = ["MAX_SIZE"];
+        let constants = syntest.constant.constants();
+
+        let mut success = false;
+        for const_name in const_to_exist.iter() {
+            constants.iter().for_each(|con| {
+                if con.ident.to_string() != *const_name {
+                    return;
+                }
+
+                assert_eq!(
+                    con.ty,
+                    syntest::parse_quote! { i32 },
+                    "Expected type i32 for constant MAX_SIZE"
+                );
+                assert_eq!(
+                    con.expr,
+                    syntest::parse_quote! { 100 },
+                    "Expected value 100 for constant MAX_SIZE"
+                );
+
+                success = true;
+            })
+        }
+
+        assert!(success, "Expected constant MAX_SIZE to exist");
+    }
+}
