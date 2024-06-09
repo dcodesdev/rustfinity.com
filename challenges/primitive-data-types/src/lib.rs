948400377b90e08572701558c4d70e525ff0c3a8
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-06-09 10:26:40 +0300

    primitive data types challenge

---
+++ challenges/primitive-data-types/src/lib.rs
@@ -0,0 +1,8 @@
+#[allow(warnings)]
+
+pub fn data_types() {
+    let x: u8 = 42;
+    let y: f64 = 3.224242422424;
+    let z: bool = false;
+    let a: char = '7';
+}
---
+++ challenges/primitive-data-types/src/starter.rs
@@ -0,0 +1,9 @@
+pub fn data_types() {
+    // 1. Define variable `x` of type `u8`
+
+    // 2. Define variable `y` of type `f64`
+
+    // 3. Define variable `z` of type `bool`
+
+    // 4. Define variable `a` of type `char`
+}
