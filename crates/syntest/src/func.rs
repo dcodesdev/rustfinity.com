33d89aabaada0d1c4f11b9e5a6c25eee3ea31fc5
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-06-10 09:53:50 +0300

    syntest visitors

--- crates/syntest/src/func.rs
+++ crates/syntest/src/func.rs
@@ -3,15 +3,16 @@ use syn::{Item, ItemFn, Stmt};
 
 pub trait Func {
     fn file(&self) -> &Rc<syn::File>;
+    fn fn_name(&self) -> &str;
 
-    fn func<F>(&self, fn_name: &str, mut handler: F)
+    fn func<F>(&self, mut handler: F)
     where
         F: FnMut(&ItemFn),
     {
         let mut ran = false;
         self.file().items.iter().for_each(|item| {
             if let Item::Fn(f) = item {
-                if f.sig.ident == fn_name {
+                if f.sig.ident == self.fn_name() {
                     handler(f);
                     ran = true;
                 }
@@ -19,16 +20,16 @@ pub trait Func {
         });
 
         if !ran {
-            panic!("Function {} not found", fn_name);
+            panic!("Function {} not found", self.fn_name());
         }
     }
 
-    fn func_stmts<F>(&self, fn_name: &str, mut handler: F)
+    fn func_stmts<F>(&self, mut handler: F)
     where
         F: FnMut(&ItemFn, &Stmt),
     {
         let mut ran = false;
-        self.func(fn_name, |f| {
+        self.func(|f| {
             f.block.stmts.iter().for_each(|stmt| {
                 handler(f, stmt);
                 ran = true;
@@ -36,7 +37,7 @@ pub trait Func {
         });
 
         if !ran {
-            panic!("No statements found for function {}", fn_name);
+            panic!("No statements found for function {}", self.fn_name());
         }
     }
 }
---
+++ crates/syntest/src/visitor.rs
@@ -0,0 +1,71 @@
+use syn::{visit::Visit, Expr, ExprCast, Type};
+
+#[derive(Debug)]
+pub struct AsVisitor {
+    as_usages: Vec<String>,
+}
+
+impl AsVisitor {
+    pub fn new(fn_name: &str, file: &syn::File) -> Self {
+        let mut visitor = Self {
+            as_usages: Vec::new(),
+        };
+
+        let function = file
+            .items
+            .iter()
+            .find_map(|item| {
+                if let syn::Item::Fn(item_fn) = item {
+                    if item_fn.sig.ident.to_string() == fn_name {
+                        Some(item_fn)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .expect(&format!("Function {fn_name} not found"));
+
+        visitor.visit_item_fn(function);
+
+        visitor
+    }
+
+    pub fn usages(&self) -> &Vec<String> {
+        &self.as_usages
+    }
+}
+
+impl<'ast> Visit<'ast> for AsVisitor {
+    fn visit_expr_cast(&mut self, expr_cast: &'ast ExprCast) {
+        if let Type::Path(path) = &*expr_cast.ty {
+            if path.path.is_ident("u32") {
+                if let Expr::Path(expr_path) = &*expr_cast.expr {
+                    self.as_usages
+                        .push(expr_path.path.segments.first().unwrap().ident.to_string());
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct FnVisitor {
+    functions: Vec<String>,
+}
+
+impl FnVisitor {
+    pub fn new() -> Self {
+        Self {
+            functions: Vec::new(),
+        }
+    }
+}
+
+impl<'ast> Visit<'ast> for FnVisitor {
+    fn visit_item_fn(&mut self, i: &'ast syn::ItemFn) {
+        let name = i.sig.ident.to_string();
+        self.functions.push(name);
+    }
+}
