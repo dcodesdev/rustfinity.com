c19e77ca636ba8547e820b5228b95ac0ce54e6bb
dcodes <101001810+dcodesdev@users.noreply.github.com>
2024-09-11 18:47:39 +0300

    playground added (#30)

--- crates/rustfinity-runner/src/command.rs
+++ crates/rustfinity-runner/src/command.rs
@@ -5,13 +5,27 @@ use std::fs;
 use std::path::Path;
 
 pub async fn run_code(code_base64: &str, challenge: &str) -> anyhow::Result<String> {
+    let code_utf8 = BASE64_STANDARD.decode(code_base64)?;
+    let code = String::from_utf8(code_utf8)?;
+
+    if challenge == "playground" {
+        let cwd = "/app/playground";
+        let main_path = Path::new(cwd).join("src/main.rs");
+
+        // Write src/main.rs
+        fs::write(&main_path, &code)?;
+
+        // Run the code
+        let output = run_command_and_merge_output("cargo", &["run"], cwd).await?;
+
+        return Ok(output);
+    }
+
+    // If you run this code outside a container, use the env var
     let challenges_path = env::var("CHALLENGES_PATH")
         // default value in container
         .unwrap_or("/app/rustfinity.com/challenges".to_string());
 
-    let code_utf8 = BASE64_STANDARD.decode(code_base64)?;
-
-    let code = String::from_utf8(code_utf8)?;
 
     let repo_path = format!("{challenges_path}/{challenge}");
     let repository_path = Path::new(&repo_path).canonicalize()?;
