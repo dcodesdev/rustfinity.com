dff3c3d930b0b9fe15459abcfb5b7305cfdf05a7
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-09-21 14:17:29 +0300

    regex update

--- crates/rustfinity-runner/src/command.rs
+++ crates/rustfinity-runner/src/command.rs
@@ -4,7 +4,25 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
+use crate::regex::extract_unittest_path;
+
 pub async fn run_code(code_base64: &str, challenge: &str) -> anyhow::Result<String> {
+    let mut output = String::new();
+
+    let tests_output = run_tests(&code_base64, &challenge).await?;
+    output.push_str(&tests_output);
+
+    output.push_str("---");
+
+    // Check time
+    let test_running_command = extract_unittest_path(&output).unwrap();
+
+    run_command_and_merge_output(&test_running_command, &[], None).await?;
+
+    Ok(output)
+}
+
+async fn run_tests(code_base64: &str, challenge: &str) -> anyhow::Result<String> {
     let code_utf8 = BASE64_STANDARD.decode(code_base64)?;
     let code = String::from_utf8(code_utf8)?;
 
@@ -16,7 +34,7 @@ pub async fn run_code(code_base64: &str, challenge: &str) -> anyhow::Result<Stri
         fs::write(&main_path, &code)?;
 
         // Run the code
-        let output = run_command_and_merge_output("cargo", &["run"], cwd).await?;
+        let output = run_command_and_merge_output("cargo", &["run"], Some(cwd)).await?;
 
         return Ok(output);
     }
@@ -25,7 +43,6 @@ pub async fn run_code(code_base64: &str, challenge: &str) -> anyhow::Result<Stri
     let challenges_path = env::var("CHALLENGES_PATH")
         // default value in container
         .unwrap_or("/app/rustfinity.com/challenges".to_string());
-
 
     let repo_path = format!("{challenges_path}/{challenge}");
     let repository_path = Path::new(&repo_path).canonicalize()?;
@@ -39,7 +56,7 @@ pub async fn run_code(code_base64: &str, challenge: &str) -> anyhow::Result<Stri
         .to_str()
         .ok_or(anyhow::anyhow!("Invalid path"))?;
 
-    let output = run_command_and_merge_output("cargo", &["test"], cwd).await?;
+    let output = run_command_and_merge_output("cargo", &["test"], Some(cwd)).await?;
 
     Ok(output)
 }
@@ -47,8 +64,10 @@ pub async fn run_code(code_base64: &str, challenge: &str) -> anyhow::Result<Stri
 pub async fn run_command_and_merge_output(
     command: &str,
     args: &[&str],
-    cwd: &str,
+    cwd: Option<&str>,
 ) -> anyhow::Result<String> {
+    let cwd = cwd.unwrap_or(".");
+
     let output = cmd!(command, args.join(" "))
         .stderr_to_stdout()
         .stdout_capture()
