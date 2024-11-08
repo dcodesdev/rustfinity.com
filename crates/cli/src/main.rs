1812a8c32ae29d1119c2fb481c11cc76314a6afb
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-11-08 11:14:07 +0300

    semver compare

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -10,17 +10,22 @@ mod editor;
 
 use clap::Parser;
 use cli::{run, Cli};
+use semver;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let latest_version = crates_io::get_latest_version().await?;
-    let current_version = env!("CARGO_PKG_VERSION");
+    let latest_version = semver::Version::parse(&latest_version)?;
 
-    if latest_version != current_version {
+    let current_version = env!("CARGO_PKG_VERSION");
+    let current_version = semver::Version::parse(current_version)?;
+
+    if latest_version > current_version {
         println!(
-            "A new version of rustfinity ({}) is available, please run the following command and try again:\n\n$ cargo install rustfinity",
+            "A new version of rustfinity ({}) is available, please run the following command and try again:",
             latest_version
         );
+        println!("$ cargo install rustfinity");
         return Ok(());
     }
 
