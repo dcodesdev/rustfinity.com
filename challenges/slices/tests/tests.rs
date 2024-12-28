152074e5c79e42f3a3823f4c4294af74e220b968
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-12-28 17:56:46 +0300

    more tests

--- challenges/slices/tests/tests.rs
+++ challenges/slices/tests/tests.rs
@@ -23,3 +23,51 @@ fn test_negative_numbers() {
     let negatives = [-10, -3, -7, -2, -99];
     assert_eq!(find_largest_in_slice(&negatives), Some(-2));
 }
+
+#[test]
+fn test_mixed_positive_and_negative() {
+    let mixed = [-10, 20, -30, 40, -50];
+    assert_eq!(find_largest_in_slice(&mixed), Some(40));
+}
+
+#[test]
+fn test_all_equal_elements() {
+    let equal = [5, 5, 5, 5];
+    assert_eq!(find_largest_in_slice(&equal), Some(5));
+}
+
+#[test]
+fn test_large_numbers() {
+    let large_numbers = [1_000_000, 2_000_000, 3_000_000];
+    assert_eq!(find_largest_in_slice(&large_numbers), Some(3_000_000));
+}
+
+#[test]
+fn test_edge_case_with_min_and_max_i32() {
+    let edge_cases = [i32::MIN, 0, i32::MAX];
+    assert_eq!(find_largest_in_slice(&edge_cases), Some(i32::MAX));
+}
+
+#[test]
+fn test_repeated_pattern_slice() {
+    let pattern = [1, 3, 2, 1, 3, 2, 1, 3, 2];
+    assert_eq!(find_largest_in_slice(&pattern), Some(3));
+}
+
+#[test]
+fn test_slice_with_zero() {
+    let with_zero = [-1, 0, 1];
+    assert_eq!(find_largest_in_slice(&with_zero), Some(1));
+}
+
+#[test]
+fn test_largest_at_end() {
+    let end_large = [10, 20, 30, 40, 50];
+    assert_eq!(find_largest_in_slice(&end_large), Some(50));
+}
+
+#[test]
+fn test_largest_at_start() {
+    let start_large = [99, 10, 20, 30];
+    assert_eq!(find_largest_in_slice(&start_large), Some(99));
+}
