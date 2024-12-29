7ca651553c927a952c46114b21022a918a5a8f57
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-12-30 01:18:42 +0300

    update

--- challenges/methods-on-structs/src/starter.rs
+++ challenges/methods-on-structs/src/starter.rs
@@ -20,3 +20,21 @@ impl Counter {
         0
     }
 }
+
+pub fn main() {
+    let mut counter = Counter::new();
+
+    counter.increment();
+    assert_eq!(counter.get_count(), 1);
+
+    counter.increment();
+    counter.increment();
+    assert_eq!(counter.get_count(), 3);
+
+    counter.decrement();
+    assert_eq!(counter.get_count(), 2);
+
+    counter.decrement();
+    counter.decrement();
+    assert_eq!(counter.get_count(), 0);
+}
