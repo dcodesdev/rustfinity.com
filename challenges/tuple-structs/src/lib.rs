fc17d34029408cf628f47718ef2edaddaac35f9d
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-12-29 22:41:29 +0300

    tests update

--- challenges/tuple-structs/src/lib.rs
+++ challenges/tuple-structs/src/lib.rs
@@ -1,5 +1,5 @@
-pub struct Rectangle(pub u32, pub u32);
+pub struct Rectangle(pub f32, pub f32);
 
-pub fn area(rect: &Rectangle) -> u32 {
+pub fn area(rect: &Rectangle) -> f32 {
     rect.0 * rect.1
 }
--- challenges/tuple-structs/tests/tests.rs
+++ challenges/tuple-structs/tests/tests.rs
@@ -2,33 +2,33 @@ use tuple_structs::*;
 
 #[test]
 fn test_area() {
-    let rect = Rectangle(4, 5);
-    assert_eq!(area(&rect), 20);
+    let rect = Rectangle(4.0, 5.0);
+    assert_eq!(area(&rect), 20.0);
 
-    let square = Rectangle(10, 10);
-    assert_eq!(area(&square), 100);
+    let square = Rectangle(10.0, 10.0);
+    assert_eq!(area(&square), 100.0);
 }
 
 #[test]
 fn test_area_zero() {
-    let flat_rect = Rectangle(0, 5);
-    assert_eq!(area(&flat_rect), 0);
+    let flat_rect = Rectangle(0.0, 5.0);
+    assert_eq!(area(&flat_rect), 0.0);
 
-    let empty_rect = Rectangle(0, 0);
-    assert_eq!(area(&empty_rect), 0);
+    let empty_rect = Rectangle(0.0, 0.0);
+    assert_eq!(area(&empty_rect), 0.0);
 }
 
 #[test]
 fn test_large_values() {
-    let large_rect = Rectangle(1_000, 2_000);
-    assert_eq!(area(&large_rect), 2_000_000);
+    let large_rect = Rectangle(1_000.0, 2_000.0);
+    assert_eq!(area(&large_rect), 2_000_000.0);
 }
 
 #[test]
 fn test_edge_cases() {
-    let tall_skinny = Rectangle(1, 100);
-    assert_eq!(area(&tall_skinny), 100);
+    let tall_skinny = Rectangle(1.0, 100.0);
+    assert_eq!(area(&tall_skinny), 100.0);
 
-    let wide_flat = Rectangle(100, 1);
-    assert_eq!(area(&wide_flat), 100);
+    let wide_flat = Rectangle(100.0, 1.0);
+    assert_eq!(area(&wide_flat), 100.0);
 }
