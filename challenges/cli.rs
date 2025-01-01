704e6f4688db8368a29a6ad940d978a4e0e723ef
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-02 01:40:39 +0300

    cli update

--- challenges/cli.rs
+++ challenges/cli.rs
@@ -1,7 +1,18 @@
-use challenges::get_max_id;
+use challenges::{challenges_json, get_max_id};
 
 fn main() {
-    let max_id = get_max_id();
+    let arg = std::env::args().nth(1).unwrap_or("".to_string());
 
-    println!("Max id is: {}", max_id);
+    match arg.as_str() {
+        "count" => {
+            let challenges = challenges_json().expect("Failed to read challenges.json");
+
+            println!("Total challenges: {}", challenges.len());
+        }
+        _ => {
+            let max_id = get_max_id();
+
+            println!("Max id is: {}", max_id);
+        }
+    }
 }
