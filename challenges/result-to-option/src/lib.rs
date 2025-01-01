dbf39991ddfdfac844ac4d15a4916c0759d75361
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-01 20:36:05 +0300

    update

--- challenges/result-to-option/src/lib.rs
+++ challenges/result-to-option/src/lib.rs
@@ -1,10 +1,3 @@
-use std::fs::File;
-use std::io::Read;
-
 pub fn read_file(file_path: &str) -> Option<String> {
-    let mut file = File::open(file_path).ok()?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).ok()?;
-
-    Some(contents)
+    std::fs::read_to_string(file_path).ok()
 }
