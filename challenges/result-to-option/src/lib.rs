bcaa406372cf497722f2bd2ffb7f0614c23a2638
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-01 20:34:27 +0300

    lib udpate

--- challenges/result-to-option/src/lib.rs
+++ challenges/result-to-option/src/lib.rs
@@ -2,8 +2,9 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn read_file(file_path: &str) -> Option<String> {
-    let mut file = File::open(file_path).ok()?; // Open the file, return None on error
+    let mut file = File::open(file_path).ok()?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents).ok()?; // Read the file content, return None on error
-    Some(contents) // Return the content as Some(String)
+    file.read_to_string(&mut contents).ok()?;
+
+    Some(contents)
 }
