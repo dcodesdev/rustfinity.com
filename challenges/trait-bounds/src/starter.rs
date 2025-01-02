4d7b2e3d9130486011a05175df893feeae07b801
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-02 15:37:46 +0300

    starter update

--- challenges/trait-bounds/src/starter.rs
+++ challenges/trait-bounds/src/starter.rs
@@ -3,11 +3,9 @@ pub fn compare_and_display // Complete the function definition
 
 // Example usage
 pub fn main() {
-    // Uncomment after implementing `compare_and_display`
+     let greater = compare_and_display(10, 20);
+     println!("Greater value: {}", greater);
 
-    // let greater = compare_and_display(10, 20);
-    // println!("Greater value: {}", greater);
-
-    // let greater = compare_and_display("Apple", "Orange");
-    // println!("Greater value: {}", greater);
+     let greater = compare_and_display("Apple", "Orange");
+     println!("Greater value: {}", greater);
 }
