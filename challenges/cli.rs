23d259d36e2c3db79d1cf2c72f9f13a48c3fe35e
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-02 15:52:48 +0300

    cli update

--- challenges/cli.rs
+++ challenges/cli.rs
@@ -1,7 +1,12 @@
+use std::{io, process::Command};
+
 use challenges::{challenges_json, get_max_id};
 
 fn main() {
-    let arg = std::env::args().nth(1).unwrap_or("".to_string());
+    let mut args = std::env::args();
+    args.next(); // Skip the first argument
+
+    let arg = args.next().unwrap_or("".to_string());
 
     match arg.as_str() {
         "count" => {
@@ -9,10 +14,34 @@ fn main() {
 
             println!("Total challenges: {}", challenges.len());
         }
+        "create" | "c" => {
+            let slug = args.next().expect("Slug is required");
+
+            Command::new("cargo")
+                .arg("new")
+                .arg("--lib")
+                .arg(slug.as_str())
+                .output()
+                .expect("Failed to create new project");
+
+            let starter_path = format!("challenges/{slug}/src/starter.rs");
+            let description_path = format!("challenges/{slug}/description.md");
+            let tests_path = format!("challenges/{slug}/tests/tests.rs");
+
+            create_file(starter_path.as_str()).expect("Failed to create starter.rs");
+            create_file(description_path.as_str()).expect("Failed to create description.md");
+            create_file(tests_path.as_str()).expect("Failed to create tests.rs");
+        }
         _ => {
             let max_id = get_max_id();
 
             println!("Max id is: {}", max_id);
         }
     }
+}
+
+fn create_file(file_path: &str) -> Result<(), io::Error> {
+    std::fs::File::create(file_path)?;
+
+    Ok(())
 }
