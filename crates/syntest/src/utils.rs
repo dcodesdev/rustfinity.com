bcb0cc6598e60a61bc578435292c8a3d59a967c9
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-03 19:12:33 +0300

    test

--- crates/syntest/src/utils.rs
+++ crates/syntest/src/utils.rs
@@ -30,3 +30,20 @@ pub fn create_bin_and_run(code: TokenStream) -> process::Output {
 
     output
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_bin_and_run() {
+        let code = quote! {
+            println!("Hello, world!");
+        };
+
+        let output = create_bin_and_run(code);
+
+        let stdout = String::from_utf8(output.stdout).unwrap();
+        assert_eq!(stdout, "Hello, world!\n");
+    }
+}
