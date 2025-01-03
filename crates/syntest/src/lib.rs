5dc0b7941efaf76cf2719f0efb854d7b997c0160
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-03 19:05:12 +0300

    update

--- crates/syntest/src/lib.rs
+++ crates/syntest/src/lib.rs
@@ -7,11 +7,13 @@ mod syntest;
 mod var;
 mod visitor;
 
-pub mod utils;
+mod utils;
 
 pub use constants::Const;
 pub use mutation::Mutation;
+pub use quote::quote;
 pub use syn::*;
 pub use syntest::Syntest;
+pub use utils::*;
 pub use var::{LocalVariable, Value};
 pub use visitor::*;
