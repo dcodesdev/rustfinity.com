c09756efc613e1436d1bc8865190773d4959a9b9
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-03 22:15:07 +0300

    update

--- challenges/lifetimes/src/starter.rs
+++ challenges/lifetimes/src/starter.rs
@@ -7,9 +7,11 @@ pub fn main() {
 
     let result = longest(s1, s2);
     println!("The longest string is: {}", result);
+    assert_eq!(result, "longer string");
 
     let s3 = "equal";
     let s4 = "equal";
     let result = longest(s3, s4);
     println!("The longest string is: {}", result);
+    assert_eq!(result, "equal");
 }
