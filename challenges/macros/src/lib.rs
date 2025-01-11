095b8de9be628fa9fb7b2b35fb45769033949eab
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-12 01:41:15 +0300

    f

--- challenges/macros/src/lib.rs
+++ challenges/macros/src/lib.rs
@@ -16,3 +16,11 @@ macro_rules! math_operations {
         format!("{} {} {} = {}", $a, $op, $b, result)
     }};
 }
+
+// Example usage
+pub fn main() {
+    assert_eq!(math_operations!(4, "+", 2), "4 + 2 = 6");
+    assert_eq!(math_operations!(10, "-", 3), "10 - 3 = 7");
+    assert_eq!(math_operations!(6, "*", 4), "6 * 4 = 24");
+    assert_eq!(math_operations!(15, "/", 3), "15 / 3 = 5");
+}
--- challenges/macros/src/starter.rs
+++ challenges/macros/src/starter.rs
@@ -3,9 +3,10 @@ macro_rules! math_operations {
     // TODO: Implement the macro
 }
 
-fn main() {
-    assert_eq!(math_operations!(4, "+", 2), "4 + 2 = 6",);
-    assert_eq!(math_operations!(10, "-", 3), "10 - 3 = 7",);
-    assert_eq!(math_operations!(6, "*", 4), "6 * 4 = 24",);
-    assert_eq!(math_operations!(15, "/", 3), "15 / 3 = 5",);
+// Example usage
+pub fn main() {
+    assert_eq!(math_operations!(4, "+", 2), "4 + 2 = 6");
+    assert_eq!(math_operations!(10, "-", 3), "10 - 3 = 7");
+    assert_eq!(math_operations!(6, "*", 4), "6 * 4 = 24");
+    assert_eq!(math_operations!(15, "/", 3), "15 / 3 = 5");
 }
