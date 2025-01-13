c7219b22904e7427d63e1ac6b08374d757190cba
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-13 10:09:46 +0300

    declaring vars

--- challenges/primitive-data-types/src/starter.rs
+++ challenges/primitive-data-types/src/starter.rs
@@ -5,7 +5,7 @@ pub fn data_types() -> (u8, f64, bool, char) {
 
     // 3. Define variable of type `bool` and value `false`
 
-    // 4. Define variable of type `char` and value `'a'`
+    // 4. Define variable of type `char` and value `a`
 
     // 5. Return a tuple with the variables in the order they were defined
 }
