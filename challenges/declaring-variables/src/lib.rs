43ebd228cfd693f4eaa7fa513a563fbb8fffeeeb
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-13 09:35:58 +0300

    declaring variables update

--- challenges/declaring-variables/src/lib.rs
+++ challenges/declaring-variables/src/lib.rs
@@ -2,5 +2,12 @@ pub fn calculate_area() -> u32 {
     let width = 10;
     let height = 5;
 
+    prints_values(width, height);
     width * height
+}
+
+// WARNING: Do not modify this function
+pub fn prints_values(width: u32, height: u32) {
+    println!("The width is: {}", width);
+    println!("The height is: {}", height);
 }
