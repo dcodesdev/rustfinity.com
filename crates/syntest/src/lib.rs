98a479bca25d5af831f1bfc9d0e9b6e7fdc788d5
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-01-13 08:54:40 +0300

    removed syntax testing

--- crates/syntest/src/lib.rs
+++ crates/syntest/src/lib.rs
@@ -1,19 +1,4 @@
-mod constants;
-mod func;
-mod mac;
-mod mutation;
-mod syn;
-mod syntest;
-mod var;
-mod visitor;
-
 mod utils;
 
-pub use constants::Const;
-pub use mutation::Mutation;
 pub use quote::quote;
-pub use syn::*;
-pub use syntest::Syntest;
 pub use utils::*;
-pub use var::{LocalVariable, Value};
-pub use visitor::*;
